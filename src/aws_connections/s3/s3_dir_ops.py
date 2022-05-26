9eb1f6e533da30f0b417993b7d7d36dd918f493a
James Kabbes <james.kabbes@gmail.com>
2022-05-25 23:22:16 -0500

    bug fix

--- src/aws_connections/s3/s3_dir_ops.py
+++ src/aws_connections/s3/s3_dir_ops.py
@@ -161,7 +161,7 @@ class S3Dir( do.RemoteDir ):
         valid = True
         for local_Path in local_Paths:
             rel_Path = local_Path.get_rel( local_Dir )
-            remote_Path = S3Path( bucket = bucket, path = do.join( path, rel_Path.path ) )
+            remote_Path = S3Path( bucket = bucket, path = do.join( path, rel_Path.path ), conn = conn )
             if not remote_Path.upload( *args, Destination = local_Path, **kwargs ):
                 valid = False
 
